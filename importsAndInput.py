f94ee224977aad0ca63ee3c4220e00161c096e67
ryanyb <ryanyb@gmail.com>
2020-12-01 19:06:35 -0600

    commit 2

--- importsAndInput.py
+++ importsAndInput.py
@@ -56,18 +56,18 @@ print()
 #       stuff above
 
 # Now changing gears slightly, we can ask the user for input through the command prompt:
-# name = input("What is your name? ")
-# quest = input("What is your quest? ")
-# color = input("What is your favorite color? ")
+#name = input("What is your name? ")
+#quest = input("What is your quest? ")
+#color = input("What is your favorite color? ")
 
 # When you run the above, you can see that python prints whatever you pass into the input function
 #  to the command line and then waits and stores whatever we put in into the variable
 # print()
 # print()
 
-# print("Your name is ", name)
-# print("Your quest is ", quest)
-# print("Your favorite color is ", color)
+#print("Your name is ", name)
+#print("Your quest is ", quest)
+#print("Your favorite color is ", color)
 
 # Now, this is kind of a fun trick, but does it work if want to do math with it?
 num = input("Enter a number: ")
@@ -75,3 +75,31 @@ num2 = input("Enter a second number: ")
 
 total = num + num2
 print("First num + second num = ", total)
+
+#No, no it don't.  We can find the problem if we print out the type of data that our input is being stored as:
+print("num is of type ", type(num))
+print("num2 is of type", type(num2))
+
+#You should see something like "num is of type <class 'str'>
+#So, python stores inputs from the input function as strings.  That's not very helpful if we want to do anything with math ever
+#So we need to change to typecast -- changing data from one type to another.
+#Computers are smart and generally can acnkowledge, as long as you tell them to, that "2" can be translated to the number 2
+#But you have to tell them to do so.
+
+numAsInt = int(num)
+num2AsInt = int(num2)
+
+# As you see above, python makes typecasting super super easy.  Just put the abbreviation for the type and then pass in the variable.
+# Other examples, str(x), float(x), char(x), etc.  
+# Now our print will work:
+
+print("Fixed first num + second num = ", numAsInt + num2AsInt)
+
+# Also notice above that Python, and just about every high level language, has a very PEMDAS-like way of processing inputs.
+# The print function above has two inputs, the string, and then (numAsInt + num2AsInt) as a single input as each aditional inpupt is 
+# written by adding a comma.  So it computes inside outward, first processing each input (the string needs no processing), numAsInt + num2AsInt, before getting 
+# processed by the print function.  Which is kinda cool, but also means that you can write really disugsting code like:
+# print(GCD(Sin(Cos(GCD(561235,Cos(205123 * Sin(29512 + GCD(20512, 29413)))))))).  But don't.
+# Use variables.  Future you reading over your code will thank you.  Like, yes you may be able to make your entire program fit into a single line of code
+# (and in some languages you have to), but don't.  Because the more code-y it looks, the more sad you will be when you have to actually read it again.
+
